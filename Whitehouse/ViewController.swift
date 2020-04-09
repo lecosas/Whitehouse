14df95ac63bd13a3dca88236882a3686f34470f8
Leandro Cosas <lecosas@gmail.com>
2020-04-09 07:35:20 -0400

    Initial Commit

---
+++ Whitehouse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whitehouse
+//
+//  Created by user163948 on 4/9/20.
+//  Copyright © 2020 lecosas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
